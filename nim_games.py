62769470fb7ecdcce36fb953ba8942c10b88b737
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-18 15:46:45 +0200

    modification 2 joueur et affichage des allumettes

--- nim_games.py
+++ nim_games.py
@@ -20,11 +20,16 @@ def supprimer_elements(tableau, nombre_elements):
         tableau.pop()
 
 
+def premier_tour():
+    print(f'Il y a 21 allumettes les voici \n {nim} \n à tour de rôle, choisissez le nombre d\'allumette à enlever,'
+          f'vous pouvez enlever 1, 2, 3 ou 4 allumettes. le dernier qui enlève la ou les allumettes à gagner')
+
+
 def partie_lance():
     while True:
         try:
             len_nim_nb = len_tableau(nim)
-            nombre_supprimer = int(input("Veuillez choisir le nombre d'allumette à enlever :"))
+            nombre_supprimer = int(input("Veuillez choisir le nombre d'allumette à enlever : "))
             if nombre_supprimer < 1 or nombre_supprimer > 4:
                 print("Veuillez entrer un nombre entre 1 et 4.")
                 continue
@@ -39,7 +44,5 @@ def partie_lance():
             print(f"Erreur : saisissez un nombre entier valide")
 
 
+premier_tour()
 partie_lance()
-
-
-
