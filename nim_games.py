6ab6571fd5162947c528883a0ecc0ad2495d22e9
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-18 14:42:59 +0200

    ajout 2nd joueur + boucle jusqu'au gagnant"

--- nim_games.py
+++ nim_games.py
@@ -14,7 +14,22 @@ def supprimer_elements(tableau, nombre_elements):
     for _ in range(nombre_elements):
         tableau.pop()
 
-
+print(nim_nb)
 nombre_a_supprimer = int(input("veuillez choisir le nombre d'allumette à enlever (1, 2, 3 ou 4) : "))
 supprimer_elements(nim_nb, nombre_a_supprimer)
-print(nim_nb)
+
+
+while True:
+    try:
+        len_nim_nb = len(nim_nb)
+        nombre_supprimer = int(input("Joueur suivant veuillez choisir le nombre "
+                                     "d'allumette à enlever (1, 2, 3 ou 4) : "))
+        supprimer_elements(nim_nb, nombre_supprimer)
+        if len_nim_nb > 1:
+            print(nim_nb)
+        else:
+            print("bravo")
+            break
+
+    except ValueError:
+        print(f"Erreur : saisissez un nombre entier valide")
