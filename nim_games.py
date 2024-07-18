b5264558d4f78114f2e22908eddfc6a69f557715
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-18 11:48:53 +0200

    Ajout du tableau et suppression element de tableau

--- nim_games.py
+++ nim_games.py
@@ -5,4 +5,16 @@
 Jeux de Nim (variante simple et de Marienbad)
 """
 
-print("jeux de nim")
+nim_nb = ["|", "|", "|", "|", "|", "|", "|",
+          "|", "|", "|", "|", "|", "|", "|",
+          "|", "|", "|", "|", "|", "|", "|"]
+
+
+def supprimer_elements(tableau, nombre_elements):
+    for _ in range(nombre_elements):
+        tableau.pop()
+
+
+nombre_a_supprimer = int(input("veuillez choisir le nombre d'allumette à enlever (1, 2, 3 ou 4) : "))
+supprimer_elements(nim_nb, nombre_a_supprimer)
+print(nim_nb)
