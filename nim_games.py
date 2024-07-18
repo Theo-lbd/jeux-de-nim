b690f332d22924a28a3ac6423ca9e8a8fd7c35d3
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-18 15:32:31 +0200

    optimisation code + function

--- nim_games.py
+++ nim_games.py
@@ -5,31 +5,41 @@
 Jeux de Nim (variante simple et de Marienbad)
 """
 
-nim_nb = ["|", "|", "|", "|", "|", "|", "|",
-          "|", "|", "|", "|", "|", "|", "|",
-          "|", "|", "|", "|", "|", "|", "|"]
+
+nim = ["|", "|", "|", "|", "|", "|", "|",
+       "|", "|", "|", "|", "|", "|", "|",
+       "|", "|", "|", "|", "|", "|", "|"]
+
+
+def len_tableau(nim_nb):
+    return len(nim_nb)
 
 
 def supprimer_elements(tableau, nombre_elements):
     for _ in range(nombre_elements):
         tableau.pop()
 
-print(nim_nb)
-nombre_a_supprimer = int(input("veuillez choisir le nombre d'allumette à enlever (1, 2, 3 ou 4) : "))
-supprimer_elements(nim_nb, nombre_a_supprimer)
+
+def partie_lance():
+    while True:
+        try:
+            len_nim_nb = len_tableau(nim)
+            nombre_supprimer = int(input("Veuillez choisir le nombre d'allumette à enlever :"))
+            if nombre_supprimer < 1 or nombre_supprimer > 4:
+                print("Veuillez entrer un nombre entre 1 et 4.")
+                continue
+            supprimer_elements(nim, nombre_supprimer)
+            if len_nim_nb > 1:
+                print(nim)
+            else:
+                print("Bravo ! Vous avez gagné.")
+                break
+
+        except ValueError:
+            print(f"Erreur : saisissez un nombre entier valide")
 
 
-while True:
-    try:
-        len_nim_nb = len(nim_nb)
-        nombre_supprimer = int(input("Joueur suivant veuillez choisir le nombre "
-                                     "d'allumette à enlever (1, 2, 3 ou 4) : "))
-        supprimer_elements(nim_nb, nombre_supprimer)
-        if len_nim_nb > 1:
-            print(nim_nb)
-        else:
-            print("bravo")
-            break
+partie_lance()
 
-    except ValueError:
-        print(f"Erreur : saisissez un nombre entier valide")
+
+
