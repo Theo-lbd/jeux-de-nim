b6a6ad28e3e7c6005849e186af2b43146602bbe2
Theo Labadie <theo.labadie@fms-ea.com>
2024-07-19 14:48:03 +0200

    Ajout des docstring

--- nim_games.py
+++ nim_games.py
@@ -12,21 +12,40 @@ nim = ["|", "|", "|", "|", "|", "|", "|",
 
 
 def len_tableau(nim_nb):
+    """
+    calcule la longueur de la liste nim.
+    :param nim_nb: Liste d'allumettes nim
+    :return: Longueur de la liste nim
+    """
     return len(nim_nb)
 
 
 def supprimer_elements(tableau, nombre_elements):
+    """
+    Supprime un nombre spécifique d'éléments
+    :param tableau: Liste des allumettes nim
+    :param nombre_elements: Nombre d'éléments à supprimer
+    :return:
+    """
     for _ in range(nombre_elements):
         tableau.pop()
 
 
 def premier_tour():
+    """
+    Affiche-les instructions du jeu
+    :return: 
+    """
     print(f"Il y a 21 allumettes, les voici : {nim}")
     print("À tour de rôle, choisissez le nombre d'allumettes à enlever.")
     print("Vous pouvez enlever 1, 2, 3 ou 4 allumettes. Le dernier qui enlève la ou les allumettes a gagné.")
 
 
 def partie_lance():
+    """
+    Démarre le jeu et gère les tours des joueurs
+    :return:
+    """
     player = 1
     while True:
         try:
